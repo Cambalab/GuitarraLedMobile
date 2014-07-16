45cbc97867891be5759484721530f69d6514948c
José Luis Di Biase <josx@interorganic.com.ar>
2014-07-16 11:38:28 -0300

    Moving out templates and controllers
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,47 +1,50 @@
 'use strict';
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('todo', ['ionic'])
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
 
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.controller('TodoCtrl', function($scope, $ionicModal) {
-  $scope.tasks = [];
-
-  // Create and load the Modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
-  }, {
-    scope: $scope,
-    animation: 'slide-in-up'
-  });
-
-  // Called when the form is submitted
-  $scope.createTask = function(task) {
-    $scope.tasks.push({
-      title: task.title
+angular
+  .module('todo', [
+    'ionic',
+    'todo.controllers',
+  ])
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.StatusBar) {
+        window.StatusBar.styleDefault();
+      }
     });
-    $scope.taskModal.hide();
-    task.title = "";
-  };
+  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('todo', {
+        url: '/todo',
+        abstract: true,
+        templateUrl: 'views/todo.html'
+      })
+      .state('todo.list', {
+        url: '/list',
+        views: {
+          'todo-list': {
+            templateUrl: 'views/todo-list.html',
+            controller: 'TodoCtrl'
+          }
+        }
+      })
+      .state('todo.about', {
+        url: '/about',
+        views: {
+          'todo-about': {
+            templateUrl: 'views/todo-about.html'
+          }
+        }
+      });
 
-  // Open our new task modal
-  $scope.newTask = function() {
-    $scope.taskModal.show();
-  };
-
-  // Close the new task modal
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  };
-});
+    $urlRouterProvider.otherwise('/todo/list');
+  });
---
+++ app/scripts/controllers/todo.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+* @ngdoc function
+* @name todo.controller:TodoCtrl
+* @description
+* # TodoCtrl
+* Controller of the todo
+*/
+
+angular.module('todo.controllers', [])
+  .controller('TodoCtrl', function($scope, $ionicModal) {
+    $scope.tasks = [];
+    $scope.task = { item: '' };
+
+    // Create and load the Modal
+    $ionicModal.fromTemplateUrl('views/new-task.html', function(modal) {
+      $scope.taskModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    // Called when the form is submitted
+    $scope.createTask = function() {
+      var item = this.task.title;
+      if(!item) { return; }
+
+      $scope.tasks.push({
+        title: item
+      });
+      $scope.taskModal.hide();
+      this.task.title = '';
+    };
+
+    // Open our new task modal
+    $scope.newTask = function() {
+      $scope.taskModal.show();
+    };
+
+    // Close the new task modal
+    $scope.closeNewTask = function() {
+      $scope.taskModal.hide();
+    };
+  });
