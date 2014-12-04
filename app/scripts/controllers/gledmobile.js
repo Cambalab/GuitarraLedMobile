bd3b30cb2fa56cb91c175e8e7f7d0741fe5dc782
Adrian Pardini <publico@tangopardo.com.ar>
2014-12-03 21:35:50 -0300

    GLedCtrl: conectarBluetooth() devuelve promise

--- app/scripts/controllers/gledmobile.js
+++ app/scripts/controllers/gledmobile.js
@@ -11,11 +11,12 @@
 angular.module('GLedMovile.controllers',[]);
 
 angular.module('GLedMovile.controllers')
-  .controller('GLedCtrl',function($scope, $ionicModal, $ionicLoading, $log, $cordovaBluetoothSerial, BluetoothService) {
+  .controller('GLedCtrl',function($scope, $ionicModal, $ionicLoading, $log, $q, $cordovaBluetoothSerial, BluetoothService) {
 
   $scope.bluetoothDevices = BluetoothService.devices
   $scope.conectarBluetooth = function(options) {
     var device = options.device;
+    var dfd = $q.defer()
 
     $ionicLoading.show({
       template: 'Conectando a ' + device.name + ' ...',
@@ -25,14 +26,17 @@ angular.module('GLedMovile.controllers')
     BluetoothService.conectar(options)
     .then( function(device) {
       $ionicLoading.hide();
+      dfd.resolve(device);
     })
-    .catch( function(device) {
+    .catch( function(device, err) {
       $ionicLoading.show({
         template: 'Bluetooth error: ' + err,
         duration: 1500,
       });
+      dfd.reject(device, err);
     });
 
+    return dfd.promise;
   }
 
   $log.debug($cordovaBluetoothSerial);
