77bd236c014f9a497c1a294a198e52337e637075
Adrian Pardini <publico@tangopardo.com.ar>
2014-12-11 16:24:12 -0300

    otra interfaz para elegir acordes

--- app/scripts/controllers/chords.js
+++ app/scripts/controllers/chords.js
@@ -10,7 +10,15 @@
 
 angular.module('GLedMovile.controllers')
   .controller('ChordsCtrl',function($scope, ChordService) {
-	  $scope.chords = ChordService.all();
+    $scope.chords = ChordService.all;
+    $scope.roots  = ChordService.roots;
+
+    $scope.selectedRoot = $scope.roots[0];
+    $scope.selectedChord = $scope.chords[$scope.selectedRoot][0];
+
+    $scope.onRootChange = function(root) {
+      $scope.selectedChord = $scope.chords[root][0];
+    }
   })
   .controller('ChordDisplayCtrl', function($scope, $stateParams, ChordService) {
     $scope.chord = ChordService.get($stateParams.chordName);
